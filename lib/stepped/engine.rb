94044f3020a53e36c561d0a5e10a2431de51d751
Robert Starsi <klevo@klevo.sk>
2025-12-13 10:16:07 +0100

    extend ActiveRecord with Stepped::Actionable

--- lib/stepped/engine.rb
+++ lib/stepped/engine.rb
@@ -1,4 +1,9 @@
 module Stepped
   class Engine < ::Rails::Engine
+    initializer "stepped.active_record.extensions" do
+      ActiveSupport.on_load :active_record do
+        include Stepped::Actionable
+      end
+    end
   end
 end
