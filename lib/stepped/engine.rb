b9d2dedd617afe30e9e03fc95fc6bc4e55907277
Robert Starsi <klevo@klevo.sk>
2025-12-13 16:21:16 +0100

    defaults

--- lib/stepped/engine.rb
+++ lib/stepped/engine.rb
@@ -1,7 +1,7 @@
 module Stepped
   class Engine < ::Rails::Engine
     config.stepped_actions = ActiveSupport::OrderedOptions.new
-    config.stepped_actions.handle_exceptions = []
+    config.stepped_actions.handle_exceptions = Rails.env.test? ? [] : [ StandardError ]
 
     initializer "stepped.active_record.extensions" do
       ActiveSupport.on_load :active_record do
