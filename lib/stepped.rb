a2fd326bee89ce7f39c2b133e4e8a3e50d155f61
Robert Starsi <klevo@klevo.sk>
2025-12-13 10:21:40 +0100

    add module methods

--- lib/stepped.rb
+++ lib/stepped.rb
@@ -2,5 +2,21 @@ require "stepped/version"
 require "stepped/engine"
 
 module Stepped
-  # Your code goes here...
+  def self.table_name_prefix
+    "stepped_"
+  end
+
+  def self.handle_exception(context: {})
+    yield
+    true
+  rescue StandardError => e
+    raise unless Rails.configuration.x.stepped_actions.handle_exceptions.any? { e.class <= _1 }
+    Rails.error.report(e, handled: false, context:)
+    false
+  end
+
+  def self.checksum(value)
+    return if value.nil?
+    Digest::SHA256.hexdigest JSON.dump(value)
+  end
 end
